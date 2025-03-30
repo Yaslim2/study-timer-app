5c9a19381c85af56f44f669b0b712a6c050902c6
Yaslim2 <yaslimsoares15@gmail.com>
2025-03-30 16:02:24 -0300

    fix: path

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/study-timer-app',
+  assetPrefix: '/study-timer-app/',
 };
 
 export default nextConfig;
