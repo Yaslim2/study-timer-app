7fb88dad27d875201458cb60ef15d71043ff8f54
Yaslim2 <yaslimsoares15@gmail.com>
2025-03-30 15:46:43 -0300

    fix: build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/study-timer-app',
+  assetPrefix: '/study-timer-app/',
 };
 
 export default nextConfig;
