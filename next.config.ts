fcca9c61bffa5eed4c27e612b278061c6e91fa32
Yaslim2 <yaslimsoares15@gmail.com>
2025-03-30 15:58:41 -0300

    fix: responsive

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/study-timer-app',
-  assetPrefix: '/study-timer-app/',
 };
 
 export default nextConfig;
